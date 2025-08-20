83b303aafb45ce34132739c2524039e31c63dc8b
Trimpsuz <me@trimpsuz.dev>
2025-08-20 22:53:33 +0300

    fix: wrong language in theme selector

--- components/FloatingControls.tsx
+++ components/FloatingControls.tsx
@@ -49,18 +49,18 @@ export default function FloatingControls({ showScroll = false }: { showScroll?: 
           <Button variant="outline" size="icon" className="cursor-pointer bg-white dark:bg-[#151515] border-[#e5e5e5] dark:border-[#383838]">
             <Sun className="h-5 w-5 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
             <Moon className="absolute h-5 w-5 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-            <span className="sr-only">Toggle theme</span>
+            <span className="sr-only">Vaihda teemaa</span>
           </Button>
         </DropdownMenuTrigger>
         <DropdownMenuContent align="end">
           <DropdownMenuItem className={`${theme === 'light' ? 'bg-muted' : ''} cursor-pointer`} onClick={() => setTheme('light')}>
-            Light
+            Vaalea
           </DropdownMenuItem>
           <DropdownMenuItem className={`${theme === 'dark' ? 'bg-muted' : ''} cursor-pointer`} onClick={() => setTheme('dark')}>
-            Dark
+            Tumma
           </DropdownMenuItem>
           <DropdownMenuItem className={`${theme === 'system' ? 'bg-muted' : ''} cursor-pointer`} onClick={() => setTheme('system')}>
-            System
+            Järjestelmä
           </DropdownMenuItem>
         </DropdownMenuContent>
       </DropdownMenu>
